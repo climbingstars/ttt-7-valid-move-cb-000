4cdff8b8052e709890892d1b87fc82b26fa51d99
Kyle Sanders <kyle.sanders.315@gmail.com>
2018-08-27 04:12:04 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -12,7 +12,7 @@ def valid_move?(board, position)
 def position_taken?(board, index)
   if board[index] == "" || board[index] == " " || board[index] == nil
     return false
-  elsif board[index] == "X" || board[index] == "O"
-    return true
+  else
+    true
   end
 end
